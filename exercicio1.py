2ea758568ae25a38ef1d339c02f0763f97bf2b85
marinaladeira <marina.ladeira@sou.inteli.edu.br>
2023-08-08 18:59:02 -0300

    ex1

--- exercicio1.py
+++ exercicio1.py
@@ -1,20 +1,15 @@
-#  Se achar necessario, faça import de outras bibliotecas
-
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def conta_palavras_unicas(words):
+    unicas = []
+    if isinstance(words, str):
+         words = words.split(' ')
+    for word in words:
+            if word not in unicas:
+                unicas.append(word) 
+    return len(unicas)
 
 # Teste a sua função aqui (caso ache necessário)
 
-
+print(conta_palavras_unicas("banana maçã banana maçã laranja maçã banana"))
 
 
 
