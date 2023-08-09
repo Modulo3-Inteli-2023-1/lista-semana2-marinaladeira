67a48ae92249d6e6c51ba868eeaa866e0b3c4443
marinaladeira <marina.ladeira@sou.inteli.edu.br>
2023-08-09 09:04:56 -0300

    ex2

--- exercicio2.py
+++ exercicio2.py
@@ -1,26 +1,15 @@
 #  Se achar necessario, faça import de outras bibliotecas
+def is_anagram(palavra1, palavra2):
+    for i in palavra1:
+        if i not in palavra2:
+            return False
+    return True
 
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
+print(is_anagram("banana", "banana"))
+print(is_anagram("banana", "maçã")) 
+print(is_anagram("amor", "roma"))
+print(is_anagram("pedra", "padre")) 
+print(is_anagram("pedra", "pedro")) 
+print(is_anagram("perda", "pedra")) 
+print(is_anagram("perda", "pedro")) 
+print(is_anagram("perda", "padre") )
